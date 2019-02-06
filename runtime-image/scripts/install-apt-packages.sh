710a591a4edeb778304632dca02f2bb9d2ccd579
sharifelgamal <selgamal@google.com>
2019-02-06 13:29:47 -0800

    explicitly run apt-get upgrade to get latest versions of packages

--- runtime-image/scripts/install-apt-packages.sh
+++ runtime-image/scripts/install-apt-packages.sh
@@ -7,6 +7,8 @@ apt-get -q update
 xargs -a <(awk '/^\s*[^#]/' '/resources/apt-packages.txt') -r -- \
     apt-get install --no-install-recommends -yq
 
+apt-get upgrade -yq
+
 # Remove unneeded files.
 apt-get clean
 rm /var/lib/apt/lists/*_*
